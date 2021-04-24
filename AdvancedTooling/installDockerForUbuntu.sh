36b2a33efca9dd3c6394fdf24e2f07758972b93e
Ben Koo <koo0905@gmail.com>
2021-04-24 23:05:59 +0800

    Added AdvancedTooling directory

---
+++ AdvancedTooling/installDockerForUbuntu.sh
@@ -0,0 +1,29 @@
+
+#! /usr/bin/bash
+
+sudo add-apt-repository universe
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    zip \
+    unzip \
+    cron \
+    dos2unix
+
+sudo dos2unix *.sh
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose

eaa742ca295b2b1e1f8117db949e8dc79bd1f677
Kevin <asd135441@gmail.com>
2021-04-27 12:17:17 +0800

    added github login; parametrized up.sh, which could be set in up.sh

--- up.sh
+++ up.sh
@@ -1,5 +1,59 @@
 #! /bin/bash
 
+
+
+
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+    # For instance, will be example_kaggle_key
+    # Assert .env has these variables
+    echo "Loaded environmental variable: TRANSPORT_STRING=$TRANSPORT_STRING"
+    echo "Loaded environmental variable: HOST_STRING=$HOST_STRING"
+    echo "Loaded environmental variable: PortNumber=$PortNumber"
+    echo "Loaded environmental variable: OAUTH_CLIENT_ID=$OAUTH_CLIENT_ID"
+    #Secret will not show
+    echo "Loaded environmental variable: OAUTH_CLIENT_SECRET=********"
+
+
+    if [[ ${TRANSPORT_STRING} =~  .*https.* ]]; then
+      echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING"
+      replaceString="$HOST_STRING";
+    else
+      echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING:$PortNumber"
+      replaceString="$HOST_STRING:$PortNumber";
+    fi
+
+
+    # Localhost configuration based on .env
+    # Substitute to correct config if founded "$TargetKey =" in LocalSetting.php
+    # Only replace 'var' instead of "var" 
+    filename="LocalSettings.php"
+    # Put in all the params for configuration
+    oauth_key_array=(
+      "wgServer" 
+      "wgOAuth2Client\[\'client\'\]\[\'id\'\]" 
+      "wgOAuth2Client\[\'client\'\]\[\'secret\'\]"
+      "wgOAuth2Client\[\'configuration\'\]\[\'redirect_uri\'\]"
+      )
+    oauth_val_array=(
+      $TRANSPORT_STRING://${replaceString} 
+      $OAUTH_CLIENT_ID 
+      $OAUTH_CLIENT_SECRET
+      "$TRANSPORT_STRING://${replaceString}/index.php/Special:OAuth2Client/callback"
+      )
+    len=${#oauth_key_array[@]}
+    for (( i=0; i<$len; i++ ));
+    do
+        sed "s|\$${oauth_key_array[$i]}[[:blank:]]*=.*|\$${oauth_key_array[$i]} = \"${oauth_val_array[$i]}\";|" $filename > temp.txt && mv temp.txt $filename
+    done
+fi
+
+
+
+# else throw exception
+
+
 # Check if docker is installed or not
 if [[ $(which docker) && $(docker --version) ]]; then
   echo "$OSTYPE has $(docker --version) installed"
@@ -50,6 +104,7 @@ DB_CONTAINER=$LOWERCASE_CURRENTDIR"_database_1"
 # stop all docker processes
 docker-compose down --volumes
 
+
 # If the mountPoint directory doesn't exist, 
 # Decompress the InitialDataPackage to ./mountPoint 
 if [ ! -e ./mountPoint/ ]; then
@@ -77,5 +132,7 @@ docker exec $MW_CONTAINER service cron start
 
 # Give read/write access to all users for the images directory.
 docker exec $MW_CONTAINER chmod -R 777 /var/www/html/images
+echo $MW_CONTAINER
+docker exec $MW_CONTAINER php /var/www/html/maintenance/update.php
 
 echo "Please go to a browser and use http://localhost:9352 to test the service"
---
+++ wk_getIn.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+LOWERCASE_CURRENTDIR="$(tr [A-Z] [a-z] <<< "${PWD##*/}")"
+
+docker exec -i -t $LOWERCASE_CURRENTDIR"_mediawiki_1" /bin/bash

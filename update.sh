32848c486f20531d52f809d597a32461bdf5e909
Kevin <asd135441@gmail.com>
2021-04-28 12:15:33 +0800

    add LocalSettings.php

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 LOWERCASE_CURRENTDIR="$(tr [A-Z] [a-z] <<< "${PWD##*/}")"
 MW_CONTAINER=$LOWERCASE_CURRENTDIR"_mediawiki_1"
 DB_CONTAINER=$LOWERCASE_CURRENTDIR"_database_1"
-docker exec $MW_CONTAINER php /var/www/html/maintenance/update.php
+docker exec $MW_CONTAINER php /var/www/html/maintenance/update.php --quick

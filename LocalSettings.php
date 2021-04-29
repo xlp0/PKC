d9a40d67bf19aa4474a84f5601a042f9da05ec4c
Ben Koo <koo0905@gmail.com>
2021-04-29 12:20:38 +0800

    Added OAuth2Client features

--- LocalSettings.php
+++ LocalSettings.php
@@ -71,6 +71,9 @@ $wgSharedTables[] = "actor";
 # Requires that a user be registered before they can edit.
 $wgGroupPermissions['*']['edit'] = false;
 
+# Prevent new user registrations except by sysops
+$wgGroupPermissions['*']['createaccount'] = false;
+
 ## Shared memory settings
 $wgMainCacheType = CACHE_ACCEL;
 $wgMemCachedServers = [];
@@ -312,3 +315,17 @@ $wgOAuth2RefreshTokenTTL="PT1M";
 
 # The following statement is only used for Semantic MediaWiki 
 enableSemantics();
+
+
+wfLoadExtension( 'MW-OAuth2Client' );
+$wgOAuth2Client['client']['id'] = "83698ede718fea93a79e";
+$wgOAuth2Client['client']['secret'] = "290648a66406e1bb3c5655a96c34b62fac5e4e9a";
+$wgOAuth2Client['configuration']['authorize_endpoint']     = 'https://github.com/login/oauth/authorize'; // Authorization URL
+$wgOAuth2Client['configuration']['access_token_endpoint']  = 'https://github.com/login/oauth/access_token'; // Token URL
+$wgOAuth2Client['configuration']['api_endpoint']           = 'https://api.github.com/user'; // URL to fetch user JSON
+$wgOAuth2Client['configuration']['redirect_uri'] = "http://localhost:9352/index.php/Special:OAuth2Client/callback";
+$wgOAuth2Client['configuration']['username'] = 'login'; // JSON path to username
+$wgOAuth2Client['configuration']['email'] = 'email'; // JSON path to email
+$wgOAuth2Client['configuration']['scopes'] = 'openid email profile'; //Permissions
+$wgOAuth2Client['configuration']['service_name'] = 'Oauth Registry'; // the name of your service
+$wgOAuth2Client['configuration']['service_login_link_text'] = 'OAuth by Github'; // the text of the login link
--- up.sh
+++ up.sh
@@ -7,20 +7,44 @@ if [ -f .env ]; then
     echo "Loaded environmental variable: TRANSPORT_STRING=$TRANSPORT_STRING"
     echo "Loaded environmental variable: HOST_STRING=$HOST_STRING"
     echo "Loaded environmental variable: PortNumber=$PortNumber"
+    echo "Loaded environmental variable: OAUTH_CLIENT_ID=$OAUTH_CLIENT_ID"
+    #Secret will not show
+    echo "Loaded environmental variable: OAUTH_CLIENT_SECRET=********"
+
+    if [[ ${TRANSPORT_STRING} =~  .*https.* ]]; then
+      echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING"
+      replaceString="$HOST_STRING";
+    else
+      echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING:$PortNumber"
+      replaceString="$HOST_STRING:$PortNumber";
+    fi
+
+
+    # Localhost configuration based on .env
+    # Substitute to correct config if founded "$TargetKey =" in LocalSetting.php
+    # Only replace 'var' instead of "var" 
+    filename="LocalSettings.php"
+    # Put in all the params for configuration
+    oauth_key_array=(
+      "wgServer" 
+      "wgOAuth2Client\[\'client\'\]\[\'id\'\]" 
+      "wgOAuth2Client\[\'client\'\]\[\'secret\'\]"
+      "wgOAuth2Client\[\'configuration\'\]\[\'redirect_uri\'\]"
+      )
+
+    oauth_val_array=(
+      $TRANSPORT_STRING://${replaceString} 
+      $OAUTH_CLIENT_ID 
+      $OAUTH_CLIENT_SECRET
+      "$TRANSPORT_STRING://${replaceString}/index.php/Special:OAuth2Client/callback"
+      )
+    len=${#oauth_key_array[@]}
+    for (( i=0; i<$len; i++ ));
+    do
+      echo "Replacing string in LocalSettings.php: ${oauth_key_array[$i]}"
+        sudo sed "s|\$${oauth_key_array[$i]}[[:blank:]]*=.*|\$${oauth_key_array[$i]} = \"${oauth_val_array[$i]}\";|" $filename > temp.txt && mv temp.txt $filename
+    done
 fi
-
-
-if [[ ${TRANSPORT_STRING} =~  .*https.* ]]; then
-	echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING"
-	replaceString="$HOST_STRING";
-else
-	echo "To use the following transport string:  ${TRANSPORT_STRING}://$HOST_STRING:$PortNumber"
-	replaceString="$HOST_STRING:$PortNumber";
-fi
-
-filename="LocalSettings.php"
-
-sudo sed "s|\$wgServer =.*|\$wgServer = \"$TRANSPORT_STRING://${replaceString}\";|" $filename > temp.txt && sudo mv temp.txt $filename
 
 echo "Please type in the Administrative(root) password of the machine that you are installing PKC service when asked... "
 
